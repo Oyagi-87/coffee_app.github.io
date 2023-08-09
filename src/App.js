ec8d34116f0588c0659ccae51405ce3bece04eee
Ken-hnntsk <u020030@st.pu-toyama.ac.jp>
2023-08-09 18:26:34 +0900

    今日の作業終わり

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import React  from 'react';
+import { useState } from 'react';
+import { render } from '@testing-library/react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  class RestCup extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        cups: 0
+      };
+    }
+  
+    Plus(prop){
+      this.setState(prevState =>({
+        cups: prevState.cups + prop
+      }));
+    }
+    
+  }
+
+  const cup = useState(0);
+
+  render();{
+    return (
+      <div className="App">
+       <h1>Coffee</h1>
+       <div>SetTime</div>
+       <div>残り{cup}杯です</div>
+       <button onClick={Minusone}>-1</button>
+       <button onClick={Minushalf}>-0.5</button>
+       <div></div>
+       <button onClick={this.Plus(2)}>+2</button>
+       <button onClick={Plusfour}>+4</button>
+       <button onClick={Plussix}>+6</button>
+       <button onClick={Pluseight}>+8</button>
+       <button onClick={Plusten}>+10</button>
+      </div>
+    );
+  }
+
+  
+}
+
+function Plustwo(prop){
+  prop = prop + 2
+  
+}
+
+function Plusfour(prop){
+    
+}
+
+function Plussix(prop){
+    
+}
+
+function Pluseight(prop){
+    
+}
+
+function Plusten(prop){
+    
+}
+
+function Minusone(prop){
+    
+}
+
+function Minushalf(prop){
+    
+}
+
+function clear(prop){
+    
 }
 
 export default App;
